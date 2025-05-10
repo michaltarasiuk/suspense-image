4767176026186179de339c748c293831ec446198
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-05-10 15:11:02 +0200

    feat: add SuspenseImage component for lazy loading images with suspense

---
+++ src/SuspenseImage.tsx
@@ -0,0 +1,21 @@
+import 'client-only';
+
+import Image, {getImageProps, type ImageProps} from 'next/image';
+import {use} from 'react';
+
+const imagePromises = new Map<string, Promise<void>>();
+
+export function SuspenseImage(props: ImageProps) {
+  const {src} = getImageProps(props).props;
+  let imagePromise = imagePromises.get(src);
+  if (!imagePromise) {
+    const {promise, resolve, reject} = Promise.withResolvers<void>();
+    const img = new window.Image();
+    img.src = src;
+    img.onload = () => resolve();
+    img.onerror = () => reject();
+    imagePromises.set(src, (imagePromise = promise));
+  }
+  use(imagePromise);
+  return <Image {...props} />;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export {add} from './utils.js';
+export {SuspenseImage} from './SuspenseImage.js';
