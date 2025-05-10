8c7c1ed34c5627a43bc85be6e78d62022c32ec63
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2025-05-10 19:36:56 +0200

    feat: enhance SuspenseImage for client-side rendering only and remove error module

--- src/SuspenseImage.tsx
+++ src/SuspenseImage.tsx
@@ -1,9 +1,16 @@
 import Image, {getImageProps, type ImageProps} from 'next/image';
-import {use} from 'react';
+import {use, useSyncExternalStore} from 'react';
 
 const imagePromises = new Map<string, Promise<void>>();
 
 export function SuspenseImage(props: ImageProps) {
+  const isSSR = useIsSSR();
+  if (isSSR) {
+    throw new Error(
+      'SuspenseImage must be used on the client side only. ' +
+        'Rendering it on the server is not supported.'
+    );
+  }
   const {src} = getImageProps(props).props;
   let imagePromise = imagePromises.get(src);
   if (!imagePromise) {
@@ -16,4 +23,21 @@ export function SuspenseImage(props: ImageProps) {
   }
   use(imagePromise);
   return <Image {...props} />;
+}
+
+function useIsSSR() {
+  const isSSR = useSyncExternalStore(
+    subscribe,
+    function getSnapshot() {
+      return false;
+    },
+    function getServerSnapshot() {
+      return true;
+    }
+  );
+  return isSSR;
+}
+
+function subscribe() {
+  return () => {};
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'tsup';
 
 export default defineConfig({
-  entryPoints: ['src/index.ts', 'src/error.ts'],
+  entryPoints: ['src/index.ts'],
   format: 'esm',
   dts: true,
   outDir: 'dist',
